5df09edeb49b0896767aacf450ab3c8840dad63e
Lockolocko <cmoirafi@gmail.com>
2020-03-05 14:19:57 +0100

    Mise aux normes checkstyle

--- src/main/java/com/gillotroux/Employe.java
+++ src/main/java/com/gillotroux/Employe.java
@@ -1,15 +1,49 @@
+/**
+ * Package de Gillot-Roux.
+ */
 package com.gillotroux;
-
+/**
+ * Classe employe.
+ * @author Lockolocko
+ *
+ */
 public class Employe {
-	protected final String nom;
-	protected final String adresse;
-	
-	public Employe(String nom,String adresse){
-		this.nom=nom;
-		this.adresse=adresse;
-	}
-	
-	public double calculeScalaire() {
-		return 1;
-	}
+/**
+ * Nom de l'employe.
+ */
+    private final String nom;
+/**
+ * Adresse de l'employe.
+ */
+    private final String adresse;
+/**
+ * Constructeur.
+ * @param nom2
+ * @param adresse2
+ */
+    public Employe(final String nom2, final String adresse2) {
+        this.nom = nom2;
+        this.adresse = adresse2;
+    }
+/**
+ * Calcule le salaire.
+ * @return 1
+ */
+    public double calculeScalaire() {
+        return 1;
+    }
+/**
+ * Retourne le nom.
+ * @return nom
+ */
+    public String getNom() {
+        return this.nom;
+    }
+/**
+ * Retourne adresse.
+ * @return adresse
+ */
+    public String getAdresse() {
+        return this.adresse;
+    }
 }
--- src/main/java/com/gillotroux/EmployePrint.java
+++ src/main/java/com/gillotroux/EmployePrint.java
@@ -1,13 +1,25 @@
+/**
+ * Package de Gillot-Roux.
+ */
 package com.gillotroux;
-
-public class EmployePrint extends Employe{
-	
-	public EmployePrint(String nom,String adresse){
-		super(nom,adresse);
-	}
-	
-	
-	public void afficheCoordonnees() {
-		System.out.println(super.nom+", "+super.adresse);
-	}
+/**
+ * Affichage des employés.
+ * @author Lockolocko
+ *
+ */
+public class EmployePrint extends Employe {
+/**
+ * Construteur.
+ * @param nom2
+ * @param adresse2
+ */
+    public EmployePrint(final String nom2, final String adresse2) {
+        super(nom2, adresse2);
+    }
+/**
+ * Affichage.
+ */
+    public void afficheCoordonnees() {
+         System.out.println(super.getNom() + ", " + super.getAdresse());
+    }
 }
